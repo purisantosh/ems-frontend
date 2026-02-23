7cd1029465645ec768f88bc6e16b6a78bf014a21
purisantosh <116016459+purisantosh@users.noreply.github.com>
2026-02-23 15:49:57 -0500

    Add Header and Footer

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,7 @@
 
 import './App.css'
+import FooterComponent from './components/FooterComponent'
+import HeaderComponent from './components/HeaderComponent'
 import ListEmployeeComponent from './components/ListEmployeeComponent'
 
 function App() {
@@ -7,7 +9,9 @@ function App() {
 
   return (
     <>
+    <HeaderComponent />
     <ListEmployeeComponent />
+    <FooterComponent />
      
     </>
   )
---
+++ src/components/FooterComponent.jsx
@@ -0,0 +1,15 @@
+import React from 'react'
+
+function FooterComponent() {
+  return (
+    <footer className='footer'>
+
+    <span>
+        All Rights Reserved 2024 @Employee Management System
+    </span>
+
+    </footer>
+  )
+}
+
+export default FooterComponent
